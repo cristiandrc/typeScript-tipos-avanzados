e2e116138daf7156dab1ad6527c0d721c12252b8
Cristian Rojas <crisdarroca@hotmail.com>
2022-09-06 21:21:09 -0500

    typles

---
+++ src/02-typles.js
@@ -0,0 +1,5 @@
+//arrays fuertemente tipados definimos que tipo de dato tiene un array y en que posicion
+var user = ["Irving", 20, true];
+var username = user[0], age = user[1];
+console.log(username);
+console.log(age);
---
+++ src/02-typles.ts
@@ -0,0 +1,6 @@
+//arrays fuertemente tipados definimos que tipo de dato tiene un array y en que posicion
+const user: [string, number, boolean] = ["Irving", 20, true]
+
+const [username,age] = user
+console.log(username)
+console.log(age)
